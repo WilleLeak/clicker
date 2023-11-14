3ca839e3285e5768969f3203ca44632bdd94a976
Evan Farnsworth <farnsworth.evan@yahoo.com>
2023-11-13 16:30:04 -0800

    changed js to look better

--- script.js
+++ script.js
@@ -57,9 +57,12 @@ var adders = {
 var upgrades = {
     mouseUpgrades:      {
         upgrade1: {
-            name: 'name1',
-            price: 0,
-            requirements: {}
+            name: 'Iron Finger',
+            price: 10,
+            requirements: {
+                amount: 5
+            },
+            effects: {}
         },
         upgrade2: {
             name: 'name1',
@@ -194,7 +197,7 @@ var upgrades = {
 };
 var totalCookiesBaked = 0;                                      // total cookies baked in game session - can ONLY increase
 var globalCount = 0;                                            // global count of all cookies
-var click = 1000000;                                                  // how much each click gives
+var click = 1;                                                  // how much each click gives
 var totalClicks = 0;                                            // total number of clicks
 var cookieCount = document.getElementById('count');             // number display for cookie
 var cookie = document.getElementById('click');                  // actual cookie to click
@@ -202,52 +205,43 @@ var cookiesPerSecond = 0;                                       // cookies baked
 var titleTab = document.getElementById('website-title-tab');    // title of the tab of the website
 var Yps = document.getElementById('YpS');                       // yarn per second element
 
-function updateCookiesDisplaySmooth() { // smoothly updates global count of cookies - no longer need updateCookieDisplay()
-    globalCount += cookiesPerSecond / 1000; // calculation for cookies per 1 ms because setInterval runs function each ms
-    if(globalCount >= 10000) {
-        cookieCount.innerHTML = Math.round(globalCount, 1); // no decimal if globalCount > 10000
+
+//                                  THESE FUNCTIONS UPDATE THE DISPLAYS IN THE GAME:
+//                                          TITLE TAB, GLOBAL COUNT, YPS
+// element - html element
+// number - number being shortened
+// messageString - string to write after the number
+const updateElementNumbers = (element, number, messageString) => {
+    if(number >= 1_000_000) { // only shorten number if >= 1 mil
+        var suffixes = [' million', ' billion', ' trillion', 'quadrillion', ' quintillion'];
+        var orderOfMagnitude = Math.floor(Math.log10(number) / 3); // this finds power of number
+        var adjustedNum = number / Math.pow(1_000, orderOfMagnitude); // this finds new number ex: 1,500,000 / 1,000,000 = 1.5
+        var formattedNum = adjustedNum.toFixed(3) + suffixes[orderOfMagnitude - 2]; // forces 3 decimals and then adds proper suffix
+        element.innerHTML =`${formattedNum} ${messageString}`;
+    } else if(number >= 10_000) {
+        element.innerHTML = `${Math.round(number, 1)} ${messageString}`;
     } else {
-        cookieCount.innerHTML = globalCount.toFixed(1); // shows decimal to one place
-    }
-}
-
-// when the cookie is clicked the global count variable is added to by how much click is
-cookie.onclick = function() {
-    globalCount += click;
-    totalClicks++;
-}
-
-const updateButtonDisplay = (button, adder) => {
-    button.innerHTML = `${adder.name} - Cost: ${adder.cost}, Amount: ${adder.amount}`; // button format for now
-};
-
-const addToGlobalCount = () => {
-    Object.values(adders).forEach(adder => { // for each to loop through each value in the adder object
-        globalCount += adder.multiplier * adder.amount; // adds to the global count
-   });
-};
-
-const calculatePerSecItems = () => {
-    var cps = 0;
-    Object.keys(adders).forEach(function(adder) { cps += adders[adder].multiplier * adders[adder].amount }); // loops through adders to add to cps
-    cookiesPerSecond = cps;
-};
-
-const updateWebsiteName = () => { 
-    if(globalCount >= 10000) {
-        titleTab.innerHTML = `${Math.round(globalCount, 1)} Strings`;
-    } else {
-        titleTab.innerHTML = `${globalCount.toFixed(1)} Strings`;
+        element.innerHTML = `${number.toFixed(1)} ${messageString}`;
     }
 };
 
-const updateYpS = () => {
-    if(cookiesPerSecond >= 10000) {
-        Yps.innerHTML = `${Math.round(cookiesPerSecond, 1)} Strings per second`;
-    } else {
-        Yps.innerHTML = `${cookiesPerSecond.toFixed(1)} Strings per second`;
-    }
+function updateCookiesDisplaySmooth() { // updates global cookie display html
+    globalCount += cookiesPerSecond / 1_000; // cookies per second / 1_000 because this runs at 1ms intervals
+    updateElementNumbers(cookieCount, globalCount, 'strings');
 }
+
+const updateWebsiteName = () => { // this updates the tab name, its just the number of strings you currently have
+    updateElementNumbers(titleTab, globalCount, 'strings');
+};
+
+const updateYpS = () => { // updates the Yps counter below the total strings
+    updateElementNumbers(Yps, cookiesPerSecond, 'strings per second');
+};
+
+//                                  END OF DISPLAY UPDATE FUNCTIONS
+
+
+//                                  THESE FUNCTIONS UPDATE THE ADDERS / ADDER BUTTONS
 
 // unified event listener for all purchase buttons instead of one function per button (i hope this does what i think im making it do)
 document.addEventListener('click', function(purchaseButton) {
@@ -259,7 +253,7 @@ document.addEventListener('click', function(purchaseButton) {
         if(globalCount >= adder.cost) {
             globalCount -= adder.cost; // purchases adder
             adder.amount++; // adds to amount
-            adder.cost = Math.round(adder.cost * 1.18, 1); // rounds cost so no decimal
+            adder.cost = Math.round(adder.cost * 1.15, 1); // rounds cost so no decimal
             updateButtonDisplay(buttonElement, adder); // updates button with info
             calculatePerSecItems(); // changes cookiesPerSec calculation
             updateYpS();
@@ -267,8 +261,51 @@ document.addEventListener('click', function(purchaseButton) {
     }
 });
 
+const updateButtonDisplay = (button, adder) => {
+    button.innerHTML = `${adder.name} - Cost: ${adder.cost}, Amount: ${adder.amount}`; // button format for now
+};
+//                                 THESE ARE ADDITION FUNCTIONS
+//                                 THEY UPDATE THE GLOBAL COUNT
+
+// when the cookie is clicked the global count variable is added to by how much click is
+cookie.onclick = function() {
+    globalCount += click;
+    totalClicks++;
+};
+
+const addToGlobalCount = () => { // this is where the global count increases
+    Object.values(adders).forEach(adder => { // for each to loop through each value in the adder object
+        globalCount += adder.multiplier * adder.amount; // adds to the global count
+   });
+};
+
+//                                 END OF ADDITION FUNCTIONS
+
+
+//                                 MISC. FUNCTIONS
+const calculatePerSecItems = () => {
+    var cps = 0;
+    Object.keys(adders).forEach(function(adder) { cps += adders[adder].multiplier * adders[adder].amount }); // loops through adders to add to cps
+    cookiesPerSecond = cps;
+};
+
+//                                 END OF MISC. FUNCTIONS
+
+//                                 UPGRADE FUNCTIONS
+//                                 FUNCTIONS THAT AFFECT UPGRADES
+const upgradeIsPurchaseable = (upgradeType, upgradeName, adder, ) => {
+    const requiredAmount = upgrades[upgradeType][upgradeName].requirements.amount;
+    if(requiredAmount >= adders[adder].amount) {
+        return false;
+    }
+    return true;
+};
+
+const upgradeClick = () => {
+
+};
 
 // all functions running continuously
 setInterval(updateCookiesDisplaySmooth, 1);
-setInterval(addToGlobalCount, 1000);
-setInterval(updateWebsiteName, 5000);
+setInterval(addToGlobalCount, 1_000);
+setInterval(updateWebsiteName, 5_000);
